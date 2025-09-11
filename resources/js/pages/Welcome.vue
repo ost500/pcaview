84aa43fb5b5afe6eba742c0e29edc3048b333af1
OST <ost5253@gmail.com>
2025-09-11 15:51:37 +0900

    card 수정

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    if (import.meta.hot) {
+        const preloader = document.getElementById('preloader');
+        if (preloader) preloader.style.display = 'none';
+    }
+});
+</script>
 
 <template>
     <!-- Preloader -->
@@ -47,11 +56,33 @@
             <div class="row" id="contentArea">
                 <div class="col-12">
                     <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">명성교회 주보</h5>
+                        </div>
                         <img src="/storage/BULLETIN/after/36.webp" class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">명성교회 2025년 9월 10일 주보</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="javascript:void(0);" class="btn btn-primary">자세히</a>
+                            <!--                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                            <p class="text-right">
+                                <a href="javascript:void(0);" class="btn btn-primary text-right">자세히</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="contentArea">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">명성교회 밝은소리</h5>
+                        </div>
+                        <img src="/storage/NEWS/after/72.webp" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">명성교회 2025년 9월 10일 밝은소리</h5>
+                            <!--                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                            <p class="text-right">
+                                <a href="javascript:void(0);" class="btn btn-primary text-right">자세히</a>
+                            </p>
                         </div>
                     </div>
                 </div>
