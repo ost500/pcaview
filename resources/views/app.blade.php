af6b707762eaac854e3c87eb3cce7e93d8d22ab7
OST <ost5253@gmail.com>
2025-09-18 14:50:58 +0900

    newsongj.com 네이버 검색

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="naver-site-verification" content="358f4b99dd28e63e78d5a4f7d986ab6cdd0df452" />
 
     {{-- Inline script to detect system dark mode preference and apply it immediately --}}
     <script>
