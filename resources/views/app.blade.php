0b5c42a347dc2478c14d4af5e8078e7222268735
OST <ost5253@gmail.com>
2025-09-22 15:18:47 +0900

    description 수정

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,12 +46,12 @@
     </style>
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    <meta name="description" content="교회의 모든 부서 주보와 소식을 한곳에 모았습니다. 함께 나누고 기억하는 우리 공동체의 이야기 공간.">
+    <meta name="description" content="교회의 모든 부서 주보와 소식을 한곳에 모았습니다. 하나님께 보고 드리는 시간 주보고가 올려 드립니다.">
 
     <meta property="og:image" content="/og_image.png?v={{ rand(100000, 999999) }}" />
     <meta property="og:title" content="<?php echo e(config('app.name', 'Laravel')); ?>" />
     <meta property="og:description"
-          content="교회의 모든 부서 주보와 소식을 한곳에 모았습니다. 함께 나누고 기억하는 우리 공동체의 이야기 공간." />
+          content="교회의 모든 부서 주보와 소식을 한곳에 모았습니다. 하나님께 보고 드리는 시간 주보고가 올려 드립니다." />
 
     <link rel="icon" href="/jubogo_favicon.png" sizes="any">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
