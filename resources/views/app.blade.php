03c9c3793290c64b753605f43e4d1b9d6dbba9b3
OST <ost5253@gmail.com>
2025-10-17 11:08:03 +0900

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -70,8 +70,16 @@
     <meta name="twitter:description" content="교회의 모든 부서 주보와 소식을 한곳에 모았습니다. 하나님께 보고 드리는 시간 주보고가 올려 드립니다." />
     <meta name="twitter:image" content="{{ url('/og_image.png') }}" />
 
-    <link rel="icon" href="/jubogo_favicon.ico" type="image/x-icon">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/jubogo_favicon.png">
+    <link rel="icon" type="image/x-icon" href="/jubogo_favicon.ico">
+    <link rel="shortcut icon" href="/jubogo_favicon.ico">
+
+    <!-- Apple Touch Icon -->
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+    <!-- PWA Manifest -->
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- Schema.org Organization -->
     <script type="application/ld+json">
