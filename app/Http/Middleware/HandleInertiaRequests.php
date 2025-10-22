b0181e5cc55ffe5b0387901f5351cdd8630e1dbb
OST <ost5253@gmail.com>
2025-10-22 17:29:16 +0900

    SEO 관련 에러 슈팅

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -60,7 +61,7 @@ class HandleInertiaRequests extends Middleware
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
             'ziggy'       => fn () => [
-                ...\Tightenco\Ziggy\Ziggy::routes($request),
+                ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
         ];
